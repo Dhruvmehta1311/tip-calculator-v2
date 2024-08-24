a28d117055795e82b4f6603df808a9d29410e977
Dhruv Mehta <dhruvmehta1311@gmail.com>
2024-08-24 20:24:26 +0530

    Fixned Calculation.

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,5 +1,6 @@
 function Total({ bill, friendServicePercentage, serviceReview }) {
-  const totalPercentage = friendServicePercentage + serviceReview;
+  const totalPercentage = (friendServicePercentage + serviceReview) / 2;
+  console.log("totalPercentage", totalPercentage);
   const finalBill = (bill * totalPercentage) / 100;
   console.log(finalBill);
 
@@ -9,7 +10,7 @@ function Total({ bill, friendServicePercentage, serviceReview }) {
   return (
     <div className="text-white">
       <p className="text-3xl ">
-        You Pay ${bill} (${bill} + ${finalBill} tip = {totalAmount})
+        You Pay ${bill} (${bill} + ${totalPercentage} tip = {totalAmount})
       </p>
     </div>
   );
