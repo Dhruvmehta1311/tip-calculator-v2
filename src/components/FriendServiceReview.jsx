f348e183408d707564a8326909a824c2ae6d4aab
Dhruv Mehta <dhruvmehta1311@gmail.com>
2024-08-24 18:22:03 +0530

    Captured Value of Friend's Review

--- src/components/FriendServiceReview.jsx
+++ src/components/FriendServiceReview.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
+
 function FriendServiceReview() {
+  const [friendServicePercentage, setFriendServicePercentage] = useState("");
+
+  const friendService = (e) => {
+    console.log(e.target.value);
+    setFriendServicePercentage(e.target.value);
+  };
+
   return (
     <div className="text-white">
       <>
         <div className="flex flex-col gap-4 md:flex-row md:items-center">
           <p>How did you liked the service?</p>
-          <select className="bg-gray-600 px-4 py-3 rounded" name="" id="">
-            <option value="">Dissatisfied (0%)</option>
-            <option value="">It was Okay (5%)</option>
-            <option value="">It was Good (10%)</option>
-            <option value="">Absolutely Amazing (100%)</option>
+          <select
+            onChange={friendService}
+            className="bg-gray-600 px-4 py-3 rounded"
+            name=""
+            id=""
+          >
+            <option value="0">Dissatisfied (0%)</option>
+            <option value="5">It was Okay (5%)</option>
+            <option value="10">It was Good (10%)</option>
+            <option value="100">Absolutely Amazing (100%)</option>
           </select>
+          <p>{friendServicePercentage}</p>
         </div>
       </>
     </div>
